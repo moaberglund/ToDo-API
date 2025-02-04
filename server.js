d146350570821f609af6a6d1d87c0f6232bf34af
moaberglund <moa.21@hotmail.com>
2025-02-04 09:42:49 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+
+// Import routes
+
+
+// Initialize express
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(bodyparser.json());
+
+// Start
+app.listen(port, () => {
+    console.log("Server running on port: " + port)
+});
+
+// Routes
+
+
+// Connect to Database - Atlas MongoDB
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.DATABASE).then(() => {
+    console.log("Connected to Atlas - MongoDB.");
+}).catch((error) => {
+    console.error("There was an error connecting to Atlas - MongoDB: " + error);
+});
